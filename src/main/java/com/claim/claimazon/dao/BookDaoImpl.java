e4ea2355b9c89a681b9a57fe40a4021021cd5ed4
Nicholas Kraftor <nicholas.p.kraftor@ehi.com>
2015-08-12 13:16:58 -0500

    Add Jetty support.  Remove XML configuration since annotations are in use. Autowire service so inner-autowiring works.

--- src/main/java/com/claim/claimazon/dao/BookDaoImpl.java
+++ src/main/java/com/claim/claimazon/dao/BookDaoImpl.java
@@ -1,72 +1,61 @@
 package com.claim.claimazon.dao;
 
+import java.util.ArrayList;
 
-import com.claim.claimazon.model.Book;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import com.claim.claimazon.model.Book;
 
 @Repository
 public class BookDaoImpl implements BookDao {
-    @Autowired
-   private  SessionFactory sessionFactory;
 
+	@Autowired
+	private SessionFactory sessionFactory;
 
-    public SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public ArrayList<Book> findAllBooks() {
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		ArrayList<Book> bookList = (ArrayList<Book>) session
+				.createCriteria(Book.class);
+		tx.commit();
+		session.close();
+		return bookList;
+	}
 
+	@Override
+	public ArrayList<Book> findBookById(String id) {
+		return null;
+	}
 
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+	@Override
+	public ArrayList<Book> findBooksByCategory(String name) {
+		return null;
+	}
 
+	@Override
+	public void updateBook(String id) {
 
+	}
 
-    }
+	@Override
+	public String deleteBook(String id) {
+		return null;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public ArrayList<Book> findAllBooks() {
-        Session session = sessionFactory.openSession();
-        Transaction tx = session.beginTransaction();
-        ArrayList<Book> bookList = (ArrayList<Book>) session.createCriteria(Book.class);
-        tx.commit();
-        session.close();
-        return bookList;
-    }
+	@Override
+	public Boolean addBook(Book book) {
+		// session = sessionFactory.openSession();
+		// tx = session.beginTransaction();
+		// session.save(book);
+		// tx.commit();
+		// session.close();
 
-    @Override
-    public ArrayList<Book> findBookById(String id) {
-        return null;
-    }
-
-    @Override
-    public ArrayList<Book> findBooksByCategory(String name) {
-        return null;
-    }
-
-    @Override
-    public void updateBook(String id) {
-
-    }
-
-    @Override
-    public String deleteBook(String id) {
-        return null;
-    }
-
-    @Override
-    public Boolean addBook(Book book) {
-//        session = sessionFactory.openSession();
-//        tx = session.beginTransaction();
-//        session.save(book);
-//        tx.commit();
-//        session.close();
-
-        return false;
-    }
+		return false;
+	}
 }
